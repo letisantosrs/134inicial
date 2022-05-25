87767e9c3c2b5f42f736061a26b3b73c98311ea6
letisantosrs <letisantosrs@gmail.com>
2022-05-24 22:13:07 -0300

    aula 24/05

--- tests/unit/test_main.py
+++ tests/unit/test_main.py
@@ -1,3 +1,5 @@
+import pytest
+
 from main import somar, dividir
 
 def teste_somar():
@@ -43,3 +45,23 @@ def teste_dividir_negativo():
 
         # 3 - Valida
         assert resultado_obtido == resultado_esperado
+
+# Lista para uso como massa de teste
+lista_de_valores = [
+    (8, 7, 15),
+    (20, 30, 50),
+    (25, 0, 25),
+    (-5, 12, 7),
+    (6, -3, 3)
+]
+
+@pytest.mark.parametrize('num1, num2, resultado_esperado', lista_de_valores)
+def teste_somar_leitura_de_lista(num1, num2, resultado_esperado):
+    # 1 - configura
+    # Utilizamos a lista como massa de teste, removendo deste trecho
+
+    # 2 - Executa
+    resultado_obtido = somar(num1, num2)
+
+    # 3 - Valida
+    assert resultado_esperado == resultado_obtido
